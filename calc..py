c2ff4b5bc3f87cfd0f552fa98241f45fc44f4c24
andrey198222 <andrejd3532@gmail.com>
2023-03-12 20:55:45 +0200

    add file

---
+++ calc..py
@@ -0,0 +1,16 @@
+
+class Calculator:
+    def multiply(self, a, b):
+        return a * b
+
+    def division(self, a, b):
+        return a / b
+
+    def subtraction(self, a, b):
+        return a - b
+
+    def adding(self, a, b):
+        return a + b
+
+    def percent(self, a, b):
+        return (a / b) * 100
